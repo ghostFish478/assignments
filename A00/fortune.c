e52aad4722880dfc784e9ce5932cd8eb40a9fcdd
ghostfish478 <bho@haverford.edu>
2022-09-01 22:21:27 -0400

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * fortune.c
+ * Author: Bethany Ho
+ * Date: 8/30/2022
+ * Make fortune
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("A journey of a thousand miles begins with a single step.\n");
+  return 0;
+}
