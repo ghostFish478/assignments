516c4606ee6862c22b23b0bf6b348ef71f968f95
ghostfish478 <bho@haverford.edu>
2022-12-09 23:56:12 -0500

    assignment complete

--- A10/buddhabrot.c
+++ A10/buddhabrot.c
@@ -7,6 +7,155 @@
 #include "read_ppm.h"
 #include "write_ppm.h"
 
+struct data {
+  int size, xcol, ycol, xrow, yrow, maxIterations, id;
+  float xmin, xmax, ymin, ymax;
+  int** buddhabrotcount;
+  struct ppm_pixel** buddhabrot;
+}
+
+void *start(void* data) {
+  pthread_barrier_t barrier;
+  threads = malloc(4 * sizeof(pthread_t));
+  buddhathread = malloc(4 * sizeof(struct data));
+  struct data *buddhathread = (struct data *) data;
+  // buddhathread = malloc(size * size * sizeof(int));
+
+  for(int a = 0; a < 4; a++) {
+    buddhathread[k].size = size;
+    buddhathread[k].xcol = xcol;
+    buddhathread[k].ycol = ycol;
+    buddhathread[k].xrow = xrow;
+    buddhathread[k].yrow = yrow;
+    buddhathread[k].maxIterations = maxIterations;
+    buddhathread[k].id = k;
+    buddhathread[k].xmin = xmin;
+    buddhathread[k].xmax = xmax;
+    buddhathread[k].ymin = ymin;
+    buddhathread[k].ymax = ymax;
+    buddhathread[k].buddhabrot = buddhabrot;
+  }
+
+  for(int i = 0; i < 4; i++) {
+    // perform step 1
+    pthread_create(&threads[i], NULL, membership, &buddhathread[i]);
+    // perform step 2
+    pthread_create(&threads[i], NULL, visitedCounts, &buddhathread[i]);
+  }
+  // use a thread barrier to wait for all threads to finish steps 1 and 2
+  pthread_barrier_wait (&barrier);
+  // perform step 3
+  for (j = 0; j < 4; j++) {
+    pthread_create(&threads[j], NULL, palette, &buddhathread[j]);
+  }
+  for (k = 0; k < 4; k++) {
+    pthread_join(threads[k], NULL);
+  }
+  return buddhathread->buddhabrot;
+}
+
+// int size, float xmin, float xmax, float ymin, float ymax, int maxIterations, struct ppm_pixel** buddhabrot
+void* membership(void* data) {
+  struct data *mandelthread = (struct data *) buddhamem;
+  //  for each row in the image
+  for(int i = 0; i < buddhamem->size; i++) {
+    //  for each col in the image
+    for(int j = 0; j < buddhamem->size; j++) {
+      float xfrac = row / buddhamem->size;
+      float yfrac = col / buddhamem->size;
+      float x0 = xmin + xfrac * (buddhamem->xmax - buddhamem->xmin);
+      float y0 = ymin + yfrac * (buddhamem->ymax - buddhamem->ymin);
+
+      x = 0;
+      y = 0;
+      int iter = 0;
+
+      while (iter < maxIterations && x*x + y*y < 4) {
+        float xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+        iter++;
+      }
+      
+      if (iter < maxIterations) {// escaped
+      // set membership at (row,col) to false
+        buddhabrotcount[j][i] = 0;
+      } else {
+      // set membership at (row,col) to true
+        buddhabrotcount[j][i] = 1;
+      }
+    }
+  }
+  return (void*)NULL;
+}
+
+void* visitedCounts(void* data) {
+  struct data *mandelthread = (struct data *) buddhamem;
+  int maxCount;
+  // for each row in the image
+  for(int i = 0; i < size; i++) {
+    // for each col in the image
+    for(int j = 0; j < size; j++) {
+      // if (row,col) belongs to the mandelbrot set, continue
+      if(buddhabrotcount[j][i] == 1) {
+        buddhabrotcount[j][i] = 0;
+        continue;
+      } else {
+        break;
+      }
+
+      float xfrac = row / size
+      float yfrac = col / size
+      float x0 = xmin + xfrac * (xmax - xmin)
+      float y0 = ymin + yfrac * (ymax - ymin)
+
+      int x = 0;
+      int y = 0;
+      while (x*x + y*y < 4) {
+        float xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+        
+        float yrow = round(size * (y - ymin)/(ymax - ymin))
+        float xcol = round(size * (x - xmin)/(xmax - xmin))
+        if (yrow < 0 || yrow >= size) continue; // out of range
+        if (xcol < 0 || xcol >= size) continue; // out of range
+
+        // increment count at (yrow, xcol)
+        buddhabrotcount[j][i] += 1;
+        // update max count
+        maxCount++;
+      }
+    }
+  }
+  return (void*)NULL;
+}
+
+void* palette(void* data) {
+  struct data *mandelthread = (struct data *) buddhamem;
+  float gamma = 0.681
+  float factor = 1.0/gamma
+  // for each row in the image
+  for(int i = 0; i < buddhamem->size; i++) {
+    // for each col in the image
+    for(int j = 0; j < buddhamem->size; j++) {
+      int value = 0;
+
+      if(buddhamem->buddhatbrotcounts[j][i] > 0) {
+        value = log(counts[row][col]) / log(max_count);
+        value = pow(value, factor);
+      }
+      struct ppm_pixel color;
+      color.red = value * 255;
+      color.green = value * 255;
+      color.blue = value * 255;
+
+      buddhamem->buddhabrot[i][j] = color;
+    }
+  }
+  return (void*)NULL;
+}
+
 int main(int argc, char* argv[]) {
   int size = 480;
   float xmin = -2.0;
@@ -34,5 +183,7 @@ int main(int argc, char* argv[]) {
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
   // todo: your code here
+  buddhabrot = start(data);
   // compute image
+  write_ppm_2d(filename, size, size, buddhabrot)
 }
--- A10/thread_mandelbrot.c
+++ A10/thread_mandelbrot.c
@@ -3,9 +3,61 @@
 #include <unistd.h>
 #include <assert.h>
 #include <time.h>
+#include <sys/time.h>
 #include <pthread.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
+
+// Generates a palette color
+struct ppm_pixel newpalette() {
+  struct ppm_pixel color;
+  color.red = rand() % 255;
+  color.green = rand() % 255;
+  color.blue = rand() % 255;
+  return color;
+}
+
+struct data {
+  // Holds variables for size, location, iterations, id, and boundaries for mandelbrot
+  int size, xcol, ycol, xrow, yrow, maxIterations, id;
+  float xmin, xmax, ymin, ymax;
+  // Holds values for palettes
+  struct ppm_pixel* palette;
+  // Holds value for canvas
+  struct ppm_pixel** mandelbrotset;
+};
+
+void *mandelbrot(void* data) {
+  struct data *mandelthread = (struct data *) data;
+  for(float i = 0; i < mandelthread->size; i++) {
+    for(float j = 0; j < mandelthread->size; j++) {
+      float xfrac = i/mandelthread->size;
+      float yfrac = j/mandelthread->size;
+      float x0 = mandelthread->xmin + xfrac * (mandelthread->xmax - mandelthread->xmin);
+      float y0 = mandelthread->ymin + yfrac * (mandelthread->ymax - mandelthread->ymin);
+
+      float x = 0;
+      float y = 0;
+      int iter = 0;
+      while (iter < mandelthread->maxIterations && x*x + y*y < 4) {
+        float xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+        iter++;
+      }
+
+      // write color to image at location (row,col)
+      if (iter < mandelthread->maxIterations) { // escaped
+        mandelthread->mandelbrotset[(int)j][(int)i] = mandelthread->palette[iter];
+      } else {
+        mandelthread->mandelbrotset[(int)j][(int)i].red = 0;
+        mandelthread->mandelbrotset[(int)j][(int)i].green = 0;
+        mandelthread->mandelbrotset[(int)j][(int)i].blue = 0;
+      }
+    }
+  }
+  return (void*)NULL;
+}
 
 int main(int argc, char* argv[]) {
   int size = 480;
@@ -15,6 +67,8 @@ int main(int argc, char* argv[]) {
   float ymax = 1.12;
   int maxIterations = 1000;
   int numProcesses = 4;
+
+  srand(time(0));
 
   int opt;
   while ((opt = getopt(argc, argv, ":s:l:r:t:b:p:")) != -1) {
@@ -34,6 +88,106 @@ int main(int argc, char* argv[]) {
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
   // todo: your code here
-  // generate pallet
+  // Timer values
+  double timer;
+  struct timeval tstart, tend;
+
+  // Create canvas for mandelbrotset
+  struct ppm_pixel** mandelbrotset = malloc(size * sizeof(struct ppm_pixel*));
+  for (int i = 0; i < size; i++) {
+    mandelbrotset[i] = malloc(size * sizeof(struct ppm_pixel));
+  }
+
+  // Holds value for a new string for the new file name for the outfile
+  char newfilename[1000 * sizeof(char)];
+
+  // Generate palette
+  struct ppm_pixel* palette= malloc(maxIterations * sizeof(struct ppm_pixel));
+  for(int j = 0; j < maxIterations; j++) {
+    palette[j] = newpalette();
+  }
+
+  // Holds value for coordinates
+  int xcol, ycol, xrow, yrow;
+  // Holds value for threads
+  pthread_t *threads;
+  // Holds value for structure for threads
+  struct data *mandelthread;
+  // Gets start time
+  gettimeofday(&tstart, NULL);
+
+  // Creates array for threads
+  threads = malloc(4 * sizeof(pthread_t));
+  // Creates array for mandelthread
+  mandelthread = malloc(4 * sizeof(struct data));
+
   // compute image
+  for (int k = 0; k < 4; k++) {
+    // Initializes variables for mandelthread
+    mandelthread[k].size = size;
+    mandelthread[k].xcol = xcol;
+    mandelthread[k].ycol = ycol;
+    mandelthread[k].xrow = xrow;
+    mandelthread[k].yrow = yrow;
+    mandelthread[k].maxIterations = maxIterations;
+    mandelthread[k].id = k;
+    mandelthread[k].xmin = xmin;
+    mandelthread[k].xmax = xmax;
+    mandelthread[k].ymin = ymin;
+    mandelthread[k].ymax = ymax;
+    mandelthread[k].palette = palette;
+    mandelthread[k].mandelbrotset = mandelbrotset;
+    // change coordinates
+    if(k == 0) {
+      xcol = 0;
+      ycol = size/2;
+      xrow = 0;
+      yrow = size/2;
+    } else if (k == 1) {
+      xcol = size/2;
+      ycol = size;
+      xrow = 0;
+      yrow = size/2;
+    } else if (k == 2) {
+      xcol = 0;
+      ycol = size/2;
+      xrow = size/2;
+      yrow = size;
+    } else if (k == 3) {
+      xcol = size/2;
+      ycol = size;
+      xrow = size/2;
+      yrow = size;
+    }
+    pthread_create(&threads[k], NULL, mandelbrot, &mandelthread[k]);
+    printf("Thread %d) sub-image block: cols (%d, %d) to rows (%d, %d)\n", mandelthread[k].id, xcol, ycol, xrow, yrow);
+  }
+
+  // Joins threads
+  for (int l = 0; l < 4; l++) {
+    pthread_join(threads[l], NULL); 
+  }
+
+  // Gets end time
+  gettimeofday(&tend, NULL);
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+
+  time_t timestamp = time(0);
+
+  // Creates the new file name
+  snprintf(newfilename, sizeof(newfilename), "mandelbrot-%d-%ld.ppm", size, timestamp);
+  printf("Computed mandelbrot set (%dx%d) in %f seconds\n", size, size, timer);
+
+  // Writes the new file
+  printf("Writing file: %s\n", newfilename);
+  write_ppm_2d(newfilename,  mandelthread->mandelbrotset, size, size);
+
+  // Frees memory
+  for(int a = 0; a < size; a++) {
+    free(mandelbrotset[a]);
+  }
+  free(mandelbrotset);
+  free(palette);
+  free(threads);
+  return 0;
 }
